6021fe00de202b9840f15e389c34c9aec9217484
Boualem HAMROUNE <kb_hamroune@esi.dz>
2025-05-31 15:43:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+
+
 
 
 
@@ -41,6 +40,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order} !', icon="✅")
 
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
     
 
 
